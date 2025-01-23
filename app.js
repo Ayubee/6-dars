d26c762abcc60e0449023c1c46541008fca26208
Ayubxon <aubsattorov@gmail.com>
2025-01-23 23:33:22 +0500

    initial commit: add basic HTML structure and placeholder JavaScript file

---
+++ app.js
@@ -0,0 +1,16 @@
+// toUpperCase()
+// toLowerCase()
+// indexOf()
+
+// let a = "156487451478951487";
+
+// if (a.indexOf("10") == a.lastIndexOf("10")) {
+//   console.log("true");
+// } else {
+//   console.log("falce");
+// }
+
+// console.log(a.includes("10"));
+
+// padStart()
+// padEnd()
